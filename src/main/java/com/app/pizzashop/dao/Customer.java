d5e1170954c5adf417ec58cc4fea839762290873
Semen Hulenko <semen.hulenko@nure.ua>
2022-07-31 18:21:38 +0300

    added customer entity/repo, new changelog

---
+++ src/main/java/com/app/pizzashop/dao/Customer.java
@@ -0,0 +1,33 @@
+package com.app.pizzashop.dao;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "customer")
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "customer_id")
+    private Long id;
+
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name")
+    private String lastName;
+
+    @Column(name = "phone")
+    private String phone;
+
+    @Column(name = "reg_date")
+    private Date regDate;
+}
